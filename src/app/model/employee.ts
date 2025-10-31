3af2d469347197ec3521a196e026866f64f06697
Sudhakar Prasad Dwivedi <sudhakkardwivedi5@gmail.com>
2025-10-31 15:50:29 +0530

    complete UI part

---
+++ src/app/model/employee.ts
@@ -0,0 +1,21 @@
+export class employeeModel{
+    empId:number;
+    name:string;
+    city:string;
+    state:string;
+    emailId:string;
+    contactNumber:string;
+    address:string;
+    pinCode:string;
+    constructor(){
+    this.address='';
+    this.city="";
+    this.contactNumber="";
+    this.emailId="";
+    this.empId=0;
+    this.name="";
+    this.state="";
+    this.pinCode="";
+    }
+}
+    
